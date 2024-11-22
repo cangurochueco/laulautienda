13bb93766550d441c2f4ea640404f9ad9a0d5a7a
cangurochueco <164103147+cangurochueco@users.noreply.github.com>
2024-11-22 20:24:06 -0300

    cuarta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from "./components/layout/navbar/Navbar";
 import { Footer } from "./components/layout/footer/Footer";
-import { Itemlistcontainer } from "./components/pages/ItemListContainer/ItemListContainergit ";
+import { Itemlistcontainer } from "./components/pages/ItemListContainer/ItemListContainer";
 function App() {
   return (
     <div>
